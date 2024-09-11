bf90d830c055fc731e0c379b393766b75afb13b5
megait <megait@gmail.com>
2024-09-11 20:58:38 +0900

    [김인규]  - 파일 첨부 기능 추가

--- src/main/java/com/marondal/memo/post/PostRestController.java
+++ src/main/java/com/marondal/memo/post/PostRestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.marondal.memo.post.domain.Post;
 import com.marondal.memo.post.service.PostService;
@@ -27,11 +28,12 @@ public class PostRestController {
 	public Map<String, String> createMemo(
 			@RequestParam("title") String title
 			, @RequestParam("contents") String contents
+			, @RequestParam(value="imageFile", required=false) MultipartFile file
 			, HttpSession session) {
 		
 		int userId = (Integer)session.getAttribute("userId");
 		
-		Post post = postService.addPost(userId, title, contents);
+		Post post = postService.addPost(userId, title, contents, file);
 		
 		Map<String, String> resultMap = new HashMap<>();
 		
